af7669c8a113ca212e9bc62e8ffad9ca76e48232
Dan Turner <55895288+thedanturner@users.noreply.github.com>
2024-03-05 08:05:12 +0000

    Initial Build

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+import csv
+import itertools
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Welcome to CSC40088 Fundamentals of Computer Science!'
+
+# new codes for Activity 3.6
+@app.route('/basic')
+def display_data():
+    with open('data/Kaggle_TwitterUSAirlineSentiment.csv', encoding='utf-8-sig') as csvfile:
+        data = csv.reader(csvfile, delimiter=',')
+        firstline = True
+        tweetData = []
+        for row in itertools.islice(data, 51):
+            if not firstline:
+                tweetData.append({
+                    "id": row[0],
+                    "airline_sentiment": row[1],
+                    "airline_sentiment_confidence": row[2],
+                    "negative_reason": row[3],
+                    "airline": row[4],
+                    "name": row[5],
+                    "text": row[6],
+                    "tweet_created": row[7],
+                    "tweet_location": row[8],
+                })
+            else:
+                firstline = False
+        return render_template('basic.html', tweetData=tweetData)
+
+
+app.run(host='0.0.0.0', port=81)
