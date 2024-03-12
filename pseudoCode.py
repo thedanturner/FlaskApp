c4895292aa566b1f2b2a6a633e4b75eeeaa99c3b
Dan Turner <55895288+thedanturner@users.noreply.github.com>
2024-03-12 07:02:22 +0000

    Added pseudoCode file

---
+++ pseudoCode.py
@@ -0,0 +1,22 @@
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+def quickSort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quickSort(left) + middle + quickSort(right)
+
+
+    # Example usage:
+    arr = [5, 2, 8, 6, 1, 9]
+    sorted_arr = quickSort(arr)
+    print(sorted_arr)
