22beb1720565c200bd6bda335749f72de45277bd
Dan Turner <55895288+thedanturner@users.noreply.github.com>
2024-03-12 07:16:30 +0000

    update pseudocode

--- pseudoCode.py
+++ pseudoCode.py
@@ -1,22 +1,27 @@
-def bubbleSort(arr):
-    n = len(arr)
-    for i in range(n-1):
-        for j in range(n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+function bubbleSort(arr):
+    n = length(arr)
+    for i from 0 to n - 1:
+        # Flag to optimize the algorithm by detecting if any swap happened in this iteration
+        swapped = False
+        for j from 0 to n - i - 1:
+            if arr[j] > arr[j + 1]:
+                # Swap if the current element is greater than the next one
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        # If no two elements were swapped in the inner loop, then the array is already sorted
+        if not swapped:
+            break
     return arr
 
-def quickSort(arr):
-    if len(arr) <= 1:
+
+function quickSort(arr):
+    if length(arr) <= 1:
         return arr
-    pivot = arr[len(arr) // 2]
-    left = [x for x in arr if x < pivot]
-    middle = [x for x in arr if x == pivot]
-    right = [x for x in arr if x > pivot]
-    return quickSort(left) + middle + quickSort(right)
-
-
-    # Example usage:
-    arr = [5, 2, 8, 6, 1, 9]
-    sorted_arr = quickSort(arr)
-    print(sorted_arr)
+    else:
+        # Select a pivot element
+        pivot = arr[0]
+        # Partition the array around the pivot
+        left = [elements in arr less than pivot]
+        right = [elements in arr greater than pivot]
+        # Recursively apply quicksort to the left and right partitions
+        return quickSort(left) + [pivot] + quickSort(right)
