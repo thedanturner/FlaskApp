0b4911ca6353affbff038915ab6242b75d6bba8c
Dan Turner <55895288+thedanturner@users.noreply.github.com>
2024-03-14 06:06:18 +0000

    Updated PseudoCode

--- pseudoCode.py
+++ pseudoCode.py
@@ -1,27 +1,25 @@
-function bubbleSort(arr):
-    n = length(arr)
-    for i from 0 to n - 1:
-        # Flag to optimize the algorithm by detecting if any swap happened in this iteration
-        swapped = False
-        for j from 0 to n - i - 1:
-            if arr[j] > arr[j + 1]:
-                # Swap if the current element is greater than the next one
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-                swapped = True
-        # If no two elements were swapped in the inner loop, then the array is already sorted
-        if not swapped:
-            break
-    return arr
+function BUBBLESORT(ARRAY)							            (1)
+    # loop through the array multiple times
+    loop INDEX from 0 to size of ARRAY – 1					    (2)
+        # consider every pair of elements except the sorted ones
+        loop INDEX2 from 0 to size of ARRAY – 2 – INDEX			(3)
+            if ARRAY[INDEX2] > ARRAY[INDEX2 + 1] then			(4)
+                # swap elements if they are out of order
+                TEMP = ARRAY[INDEX2]						    (5)
+                ARRAY[INDEX2] = ARRAY[INDEX2 + 1]				(6)
+                ARRAY[INDEX2 + 1] = TEMP					    (7)
+            end if
+        end loop
+    end loop
+end function
 
 
-function quickSort(arr):
-    if length(arr) <= 1:
-        return arr
-    else:
-        # Select a pivot element
-        pivot = arr[0]
-        # Partition the array around the pivot
-        left = [elements in arr less than pivot]
-        right = [elements in arr greater than pivot]
-        # Recursively apply quicksort to the left and right partitions
-        return quickSort(left) + [pivot] + quickSort(right)
+function QUICKSORT(ARRAY, START, END)			(1)
+    # base case size <= 1		
+    if START >= END then					    (2)		
+        return						            (3)
+    end if							            (4)
+    PIVOTINDEX = PARTITION(ARRAY, START, END)	(5)
+    QUICKSORT(ARRAY, START, PIVOTINDEX – 1)		(6)
+    QUICKSORT(ARRAY, PIVOTINDEX + 1, END)		(7)
+end function
