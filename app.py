bc387a91f11852cc35b78e2116cd71a4f7ca65a8
Dan Turner <55895288+thedanturner@users.noreply.github.com>
2024-03-21 06:38:05 +0000

    Made changes for Activity 3.7

--- app.py
+++ app.py
@@ -9,13 +9,14 @@ app = Flask(__name__)
 def index():
     return 'Welcome to CSC40088 Fundamentals of Computer Science!'
 
+
 @app.route('/basic')
 def display_data():
     with open('data/Kaggle_TwitterUSAirlineSentiment.csv', encoding='utf-8-sig') as csvfile:
         data = csv.reader(csvfile, delimiter=',')
         firstline = True
         tweetData = []
-        for row in itertools.islice(data, 51):
+        for row in itertools.islice(data, 41):
             if not firstline:
                 tweetData.append({
                     "id": row[0],
@@ -30,7 +31,12 @@ def display_data():
                 })
             else:
                 firstline = False
+
+        # Sort the data by airline_sentiment_confidence
+                
+        tweetData.sort(key=lambda x: x["airline_sentiment_confidence"])
+        
+        # Return the data to the template
         return render_template('basic.html', tweetData=tweetData)
-
-
+    
 app.run(host='0.0.0.0', port=81)
