fba1accbfe6fe9255ac27a984f2d8915098c84ed
Dan Turner <55895288+thedanturner@users.noreply.github.com>
2024-03-23 06:33:37 +0000

    Added additional test

--- bubble_sort_test.py
+++ bubble_sort_test.py
@@ -11,5 +11,11 @@ class TestBubbleSort(unittest.TestCase):
     def test_bubble_sort_empty_list(self):
         self.assertEqual(bubble_sort([]), [])
 
+    def test_bubble_sort_with_negative_numbers(self):
+        self.assertEqual(bubble_sort([-5,-5,-7,-8,-2,-4,-1]), [-8,-7,-5,-5,-4,-2,-1])
+
+    def test_bubble_sort_with_mixed_numbers(self):
+        self.assertEqual(bubble_sort([5,-5,7,-8,2,-4,1]), [-8,-5,-4,1,2,5,7])
+        
 if __name__ == '__main__':
     unittest.main(verbosity=2)
