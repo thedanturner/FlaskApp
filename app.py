817b5c324bc35d9550c56bbca05246094b99450b
Dan Turner <danielturner@outlook.com>
2024-04-25 07:41:50 +0100

    added websocket function

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
 import csv
 import itertools
 import os
+from flask import Flask, render_template, session, copy_current_request_context
+from flask_socketio import SocketIO, emit, disconnect
+from threading import Lock
+async_mode = None
 
 app = Flask(__name__)
+socket_ = SocketIO(app, async_mode=async_mode)
 
 
 @app.route('/')
@@ -53,7 +57,40 @@ def display_creative_data():
 def report():
     return render_template("report.html")
 
+@app.route('/chat')
+def chat():
+    return render_template("chat.html",
+                           sync_mode=socket_.async_mode)
+
+@socket_.on('my_event', namespace='/test')
+def test_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']})
+
+
+@socket_.on('my_broadcast_event', namespace='/test')
+def test_broadcast_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']},
+         broadcast=True)
+
+
+@socket_.on('disconnect_request', namespace='/test')
+def disconnect_request():
+    @copy_current_request_context
+    def can_disconnect():
+        disconnect()
+
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': 'Disconnected!', 'count': session['receive_count']},
+         callback=can_disconnect)
+    
 # app.run(host='0.0.0.0', port=81, debug=True)
 
-port = int(os.environ.get('PORT', 81))
-app.run(host='0.0.0.0', port=port)
+# port = int(os.environ.get('PORT', 81))
+# app.run(host='0.0.0.0', port=port)
+
+socket_.run(app, debug=True)
